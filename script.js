34f9d391950cce3aaed7cd5e25b3e51b21aacd3e
michael <michaeloladele373@gmail.com>
2025-01-31 11:41:07 +0100

    changes

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const timeElement = document.querySelector('[data-testid="currentTimelocal"]');
+const timeElement = document.querySelector('.time');
 function updateTime() {
   const now = new Date();
-  timeElement.textContent = `Current localTime: ${now.toTimeString}`;
+  timeElement.textContent = `Current localTime: ${now.toLocaleTimeString()}`;
 }
 updateTime();
-setInterval(updateTime, 60000); // Update every minute
+setInterval(updateTime, 1000); // Update time every second
